0b4ec57833bc63f556d82c2b830d78c2eaa57d1a
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-23 22:11:44 +0100

    Fleeing Part 2
    Finished fleeing, changed the code so that speed doesnt constantly increase after a new obstacle.

--- Assets/DropCilinder.cs
+++ Assets/DropCilinder.cs
@@ -23,10 +23,10 @@ public class DropCilinder : MonoBehaviour
             if(Physics.Raycast(ray.origin, ray.direction, out hitInfo))
             {
                 Instantiate(obstacle, hitInfo.point, obstacle.transform.rotation);
-                //foreach(GameObject a in agents)
-                //{
-
-                //}
+                foreach(GameObject a in agents)
+                {
+                    a.GetComponent<MyAIControl>().DetectNewObstacle(hitInfo.point);
+                }
             }
         }
     }
--- Assets/Scripts/MyAIControl.cs
+++ Assets/Scripts/MyAIControl.cs
@@ -9,6 +9,10 @@ public class MyAIControl : MonoBehaviour
     GameObject[] goalLocations;
     NavMeshAgent agent;
     Animator anim;
+    float speedMult;
+    float detectionRadius = 20;
+    float fleeRadius = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +22,42 @@ public class MyAIControl : MonoBehaviour
         int i = Random.Range(0, goalLocations.Length);
         agent.SetDestination(goalLocations[i].transform.position);
 
-        anim.SetTrigger("isWalking");
         anim.SetFloat("wOffset", Random.Range(0,1));
-        float sm = Random.Range(0.5f, 2);
-        anim.SetFloat("speedMult", sm);
-        agent.speed *= sm;
+
+        ResetAgent();
+    }
+
+    void ResetAgent()
+    {
+        speedMult = Random.Range(0.5f, 2);
+        anim.SetFloat("speedMult", speedMult);
+        agent.speed = 2 * speedMult;
+
+        anim.SetTrigger("isWalking");
+
+        agent.angularSpeed = 120;
+
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if(Vector3.Distance(position, transform.position) < detectionRadius)
+        {
+            Vector3 fleeDir = (transform.position - position).normalized;
+            Vector3 newGoal = transform.position + fleeDir * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newGoal, path);
+
+            if(path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10;
+                agent.angularSpeed = 500;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -30,6 +65,7 @@ public class MyAIControl : MonoBehaviour
     {
         if (agent.remainingDistance < 1)
         {
+            ResetAgent();
             int i = Random.Range(0, goalLocations.Length);
             agent.SetDestination(goalLocations[i].transform.position);
         }
